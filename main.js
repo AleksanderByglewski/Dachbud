a36c4169ec69ce1d5a725d729a906e236b68063a
Aleksander <alexbyglewski@icloud.com>
2021-12-21 14:57:13 +0100

    working on the garage object

--- main.js
+++ main.js
@@ -4,20 +4,61 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+class General_object{
+ show_me_desire(){
+  alert("Hello")}
 
+}
+class Foundation extends General_object{
+  
+  constructor(width, depth){
+  super();
+  this.components=[];
+  this.width=width;
+  this.depth=depth;
+    }
+  add_components_to_scene(scene){
+    const geometry = new THREE.TorusGeometry(2, 0.5, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0xff6347,wireframe: true });
+    const torus = new THREE.Mesh(geometry, material);
+    scene.add(torus);
+  }
+}
+
+
+
+class Garage_block extends General_object{
+  
+  constructor(width, depth){
+  super();
+  this.components=[];
+  this.width=width;
+  this.depth=depth;
+    }
+  add_components_to_scene(scene){
+    const geometry = new THREE.TorusGeometry(2, 0.5, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0xff6347,wireframe: true });
+    const torus = new THREE.Mesh(geometry, material);
+    scene.add(torus);
+  }
+}
+
+
+
+function main(){
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
+let flat_ground=new Foundation(7.5,14)
+flat_ground.add_components_to_scene(scene)
+
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setY(20);
-
 camera.rotateX(0.75)
 
 renderer.render(scene, camera);
@@ -27,8 +68,18 @@ renderer.render(scene, camera);
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xff6347,wireframe: true });
 const torus = new THREE.Mesh(geometry, material);
-
 scene.add(torus);
+
+
+
+//Plane
+
+const geometry_plane = new THREE.PlaneGeometry( 14, 8 );
+geometry_plane.rotateX(Math.PI/2)
+const material_plane = new THREE.MeshBasicMaterial( {color: 0xff6347, side: THREE.DoubleSide} );
+const plane = new THREE.Mesh( geometry_plane, material_plane );
+scene.add( plane );
+
 
 // Lights
 
@@ -41,7 +92,7 @@ scene.add(pointLight, ambientLight);
 // Helpers
 
  const lightHelper = new THREE.PointLightHelper(pointLight)
- const gridHelper = new THREE.GridHelper(200, 50);
+ const gridHelper = new THREE.GridHelper(200, 200,200);
  scene.add(lightHelper, gridHelper)
 
  const controls = new OrbitControls(camera, renderer.domElement);
@@ -49,20 +100,16 @@ scene.add(pointLight, ambientLight);
 
 // Background
 
-
-
 // Scroll Animation
-
-
 
 // Animation Loop
 
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  //torus.rotation.x += 0.01;
+  //torus.rotation.y += 0.005;
+  //torus.rotation.z += 0.01;
 
 
   // controls.update();
@@ -70,7 +117,19 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+}
+main()
 
+
+
+function helper_fuction(){
+  document.addEventListener('keydown', logKey);
+  
+  function logKey(e) {
+    console.log( ` ${e.code}`);
+  }
+}
+helper_fuction();
 
 
 
