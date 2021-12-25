db94966afec5e34f4ecccae335285d76266064ea
alexbyglewski <alexbyglewski@icloud.com>
2021-12-25 11:26:50 +0100

    Finished the frontend of the menu now for the backend

--- HTMLcomponents/side-menu/side-menu.js
+++ HTMLcomponents/side-menu/side-menu.js
@@ -6,16 +6,32 @@ document.querySelectorAll(".menu-elem-heading").forEach(item => {
 document.querySelector(".menu-elem-heading").addEventListener("click", reduct)
 function reduct(ele){
  
-    let m=ele.currentTarget.nextElementSibling
+
+
+
     
+    let m=ele.currentTarget.nextElementSibling
     m.classList.toggle("hide")
     but = ele.currentTarget.querySelector("img")
     if (m.classList.contains("hide")){
     but.src="./favicons/plus.svg"
+        
     }
-    else{
-        but.src="./favicons/minus.svg"
-    }
+    else
+    {
+    but.src="./favicons/minus.svg"
+       
+}
+    
+
+    if (m.style.maxHeight) {
+
+        m.style.maxHeight = null;
+      } else {
+        m.style.maxHeight = m.scrollHeight + "px";
+      }
+
+    
   //  alert("hello")
 }
 
